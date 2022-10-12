d7a1e87046585faebaa225a6a1e8136f0e654cb5
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2022-10-11 21:57:27 -0500

    add JWT_SECRET

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const JWT_SECRET = 'supersecret'
-
+const {JWT_SECRET} = process.env
 function sign(payload){
     return jwt.sign(payload, JWT_SECRET, {expiresIn: '1d'})
 }
