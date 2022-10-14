fec22f2fa59b36b09495f5d4183f9bfe0698d369
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2022-10-14 11:44:44 -0500

    handle error middleware

--- src/middlewares/errorHandle.js
+++ src/middlewares/errorHandle.js
@@ -1,8 +1,17 @@
-function errorHandle(error, request, response, next) {
-    response.status(error.status).json({
-        success: false,
+// Una aplicación de manejo de errores con un middleware:
+export function handleErrors(error, request, response, next) {
+    // console.log(err);
+    // res.status(500).send('An internal server error occurred');
+    console.log('======')
+    console.log(error.name)
+    console.log(error.status)
+    console.log(error.message)
+    if(error.name === 'ValidationError') {
+        response.status(400).json({success: false, message: error.message})
+        return
+    }
+    response.status(error.status || 500).json({
+        success: false, 
         message: error.message
     })
 }
-
-export {errorHandle}
